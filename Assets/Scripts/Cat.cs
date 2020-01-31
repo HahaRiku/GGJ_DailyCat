ec7b0af321f9de4e32fd4ecf6612bc3550332704
HahaRiku <s9323839@gmail.com>
2020-02-01 01:31:27 +0800

    [新增]Cat Script 未完成

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour {
+    public enum Direction {
+        上下,
+        左右
+    }
+    private Direction dir;
+    private float positiveValue;    //up and right
+    private float negativeValue;    //down and left
+    private bool start = false;
+    
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(start) {
+            if(dir == Direction.上下) {
+
+            }
+            else {
+
+            }
+        }
+    }
+
+    public void SetCat(Direction d, float pv, float nv) {
+        dir = d;
+        pv = positiveValue;
+        nv = negativeValue;
+    }
+    
+    public void StartMove() {
+        start = true;
+    }
+
+    public void ByeCat() {
+        start = false;
+    }
+}
