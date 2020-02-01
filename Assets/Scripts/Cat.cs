cb3d0d3756372e6efc71c7951462b337e1e02abd
HahaRiku <s9323839@gmail.com>
2020-02-01 12:08:40 +0800

    貓的移動方式大概好了ㄅ(?

--- Assets/Scripts/Cat.cs
+++ Assets/Scripts/Cat.cs
@@ -20,7 +20,7 @@ public class Cat : MonoBehaviour {
     private Direction dir;
     private float upOrRightMaxValue;    //up and right
     private float downOrLeftMaxValue;    //down and left
-    public Status status;
+    private Status status;
     private float speed;
 
     private bool start = false;
@@ -31,39 +31,42 @@ public class Cat : MonoBehaviour {
     private FourDirection helloDir;
     private float helloDestX;
     private float helloDestY;
-    private string name;
 
     void Update() {
         if(hello) {
             if (helloDir == FourDirection.上) {
-                transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y + speed * 2);
+                transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y + speed * 5);
                 if (transform.localPosition.y >= helloDestY) {
                     hello = false;
+                    start = true;
                 }
             }
             else if (helloDir == FourDirection.下) {
-                transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y - speed * 2);
+                transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y - speed * 5);
                 if (transform.localPosition.y <= helloDestY) {
                     hello = false;
+                    start = true;
                 }
             }
             else if (helloDir == FourDirection.右) {
-                transform.localPosition = new Vector2(transform.localPosition.x + speed * 2, transform.localPosition.y);
+                transform.localPosition = new Vector2(transform.localPosition.x + speed * 5, transform.localPosition.y);
                 if (transform.localPosition.x >= helloDestX) {
                     hello = false;
+                    start = true;
                 }
             }
             else {
-                transform.localPosition = new Vector2(transform.localPosition.x - speed * 2, transform.localPosition.y);
+                transform.localPosition = new Vector2(transform.localPosition.x - speed * 5, transform.localPosition.y);
                 if (transform.localPosition.x <= helloDestX) {
                     hello = false;
+                    start = true;
                 }
             }
         }
         else if(start) {
             if(dir == Direction.上下) {
                 if(status == Status.正方向) {
-                    if(transform.localPosition.y < upOrRightMaxValue - speed) {
+                    if(transform.localPosition.y <= upOrRightMaxValue - speed) {
                         transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y + speed);
                     }
                     else {
@@ -72,19 +75,20 @@ public class Cat : MonoBehaviour {
                     }
                 }
                 else {
-                    if (transform.localPosition.y > downOrLeftMaxValue + speed) {
+                    if (transform.localPosition.y >= downOrLeftMaxValue + speed) {
                         transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y - speed);
                     }
                     else {
                         status = Status.正方向;
-                        transform.localPosition = new Vector2(transform.localPosition.x, upOrRightMaxValue + speed - transform.localPosition.y + upOrRightMaxValue);
+                        transform.localPosition = new Vector2(transform.localPosition.x, downOrLeftMaxValue + speed - transform.localPosition.y + downOrLeftMaxValue);
                     }
                 }
             }
             else {
                 if (status == Status.正方向) {
-                    if (transform.localPosition.x < upOrRightMaxValue - speed) {
-                        transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.x + speed);
+                    if (transform.localPosition.x <= upOrRightMaxValue - speed) {
+                        print("test2");
+                        transform.localPosition = new Vector2(transform.localPosition.x + speed, transform.localPosition.y);
                     }
                     else {
                         status = Status.負方向;
@@ -92,37 +96,38 @@ public class Cat : MonoBehaviour {
                     }
                 }
                 else {
-                    if (transform.localPosition.x > downOrLeftMaxValue + speed) {
-                        transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.x - speed);
+                    if (transform.localPosition.x >= downOrLeftMaxValue + speed) {
+                        print("test");
+                        transform.localPosition = new Vector2(transform.localPosition.x - speed, transform.localPosition.y);
                     }
                     else {
                         status = Status.正方向;
-                        transform.localPosition = new Vector2(upOrRightMaxValue + speed - transform.localPosition.x + upOrRightMaxValue, transform.localPosition.y);
+                        transform.localPosition = new Vector2(downOrLeftMaxValue + speed - transform.localPosition.x + downOrLeftMaxValue, transform.localPosition.y);
                     }
                 }
             }
         }
         else if(bye) {
             if(byeDir == FourDirection.上) {
-                transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y + speed * 2);
+                transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y + speed * 5);
                 if(transform.localPosition.y >= 6) {
                     bye = false;
                 }
             }
             else if(byeDir == FourDirection.下) {
-                transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y - speed * 2);
+                transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y - speed * 5);
                 if (transform.localPosition.y <= -6) {
                     bye = false;
                 }
             }
             else if(byeDir == FourDirection.右) {
-                transform.localPosition = new Vector2(transform.localPosition.x + speed * 2, transform.localPosition.y);
+                transform.localPosition = new Vector2(transform.localPosition.x + speed * 5, transform.localPosition.y);
                 if (transform.localPosition.x >= 10) {
                     bye = false;
                 }
             }
             else {
-                transform.localPosition = new Vector2(transform.localPosition.x - speed * 2, transform.localPosition.y);
+                transform.localPosition = new Vector2(transform.localPosition.x - speed * 5, transform.localPosition.y);
                 if (transform.localPosition.x <= -10) {
                     bye = false;
                 }
@@ -130,37 +135,39 @@ public class Cat : MonoBehaviour {
         }
     }
 
-    public void SetCat(Direction d, float p, float n, float speed, string name) {
+    public void SetCat(Direction d, float p, float n, float speed, string name, float destX, float destY) {
         dir = d;
-        float temp = ((d == Direction.上下) ? transform.localPosition.y : transform.localPosition.x);
+        float temp = ((d == Direction.上下) ? destY : destX);
         upOrRightMaxValue = temp + p;
         downOrLeftMaxValue = temp - n;
         this.speed = speed;
         status = (Random.Range(0, 2) == 0) ? Status.正方向 : Status.負方向;
         this.name = name;
+        helloDestX = destX;
+        helloDestY = destY;
+        if (Mathf.Abs(helloDestX) >= Mathf.Abs(helloDestY)) {
+            helloDir = (helloDestX >= 0) ? FourDirection.左 : FourDirection.右;
+            transform.localPosition = (helloDestX >= 0) ? new Vector2(10, destY) : new Vector2(-10, destY);
+        }
+        else {
+            helloDir = (helloDestY >= 0) ? FourDirection.下 : FourDirection.上;
+            transform.localPosition = (helloDestY >= 0) ? new Vector2(destX, 6) : new Vector2(destX, -6);
+        }
     }
 
     public void ByeCat() {
         start = false;
-        bye = true;
         if(Mathf.Abs(transform.localPosition.x) >= Mathf.Abs(transform.localPosition.y)) {
             byeDir = (transform.localPosition.x >= 0) ? FourDirection.右 : FourDirection.左;
         }
         else {
             byeDir = (transform.localPosition.y >= 0) ? FourDirection.上 : FourDirection.下;
         }
+        bye = true;
     }
 
-    public void HelloCat(float destX, float destY) {
+    public void HelloCat() {
         hello = true;
-        helloDestX = destX;
-        helloDestY = destY;
-        if (Mathf.Abs(destX) >= Mathf.Abs(destY)) {
-            helloDir = (destX >= 0) ? FourDirection.左 : FourDirection.右;
-        }
-        else {
-            helloDir = (destY >= 0) ? FourDirection.下 : FourDirection.上;
-        }
     }
 
     public bool IsIdle() {
@@ -168,6 +175,6 @@ public class Cat : MonoBehaviour {
     }
 
     public bool IsExisted() {
-        return start && bye;
+        return start || bye || hello;
     }
 }
