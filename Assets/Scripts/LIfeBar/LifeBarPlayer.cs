c4b39e12fc87e8855360770fd8009b711cd0ab88
kn71026 <nicolechou9712@gmail.com>
2020-02-01 20:16:59 +0800

    Add player animation

---
+++ Assets/Scripts/LIfeBar/LifeBarPlayer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeBarPlayer : MonoBehaviour
+{
+    Animator anim;
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    //player moving range
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+        anim = GetComponent<Animator>();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        transform.position = new Vector3(newXPos, newYPos,-3);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            anim.SetInteger("dir", 3);
+            anim.SetBool("isWalking", true);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetInteger("dir", 0);
+            anim.SetBool("isWalking", true);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetInteger("dir", 2);
+            anim.SetBool("isWalking", true);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("dir", 1);
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+        //if(Input)
+    }
+}
