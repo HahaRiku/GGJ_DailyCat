df558fe885dca617f3782d4bb60f2b2bf0e8441a
uccu <justin2000721@gmail.ocm>
2020-02-02 09:55:12 +0800

    title fix

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
